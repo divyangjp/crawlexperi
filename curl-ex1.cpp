c4a5add7ddb901e34998a607ffc08af6dbae3833
Divyang Patel <divyangpatel009@gmail.com>
2015-06-03 14:41:01 +1000

    minor

--- curl-ex1.cpp
+++ curl-ex1.cpp
@@ -27,7 +27,7 @@ int main(void)
   curl = curl_easy_init();
   if(curl) {
     //curl_easy_setopt(curl, CURLOPT_URL, "http://curl.haxx.se/libcurl/");
-	curl_easy_setopt(curl, CURLOPT_URL, "timesofindia.indiatimes.com");
+	curl_easy_setopt(curl, CURLOPT_URL, "http://www.news.com.au");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
     res = curl_easy_perform(curl);
